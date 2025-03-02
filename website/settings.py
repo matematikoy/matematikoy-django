d7690c60c0e27ecc5ca51a020e482fdf73d9f7bc
matematikoy <lucasmarques.mat@gmail.com>
2025-03-02 19:48:57 -0300

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 import os
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-
+# Base directory do projeto
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -134,8 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 
-# Base directory do projeto
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # URL dos arquivos estáticos
 STATIC_URL = '/static/'
