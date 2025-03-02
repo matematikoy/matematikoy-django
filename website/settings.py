82e21698ee607c3178301e5f548f8f44f450e254
matematikoy <lucasmarques.mat@gmail.com>
2025-03-02 18:11:52 -0300

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Diretório onde os arquivos estáticos serão coletados para produção
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
