8a5d6d084bcbf7c1c616409b503562380839a223
matematikoy <lucasmarques.mat@gmail.com>
2025-03-02 19:14:38 -0300

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -141,7 +141,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),  # Onde estão os arquivos antes de serem coletados
 ]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.StaticFilesStorage"
 
 
 
